b8a882518f2bc2168f79e06d0d68a6766f40d178
yulia <langedoc@gmail.com>
2024-11-28 19:14:16 +0100

    feat: insert message, get iban code without not numerical or not alphabetical simbols

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [input, setInput] = useState('');
+  const handleClick = () => {
+    const inputElement = document.querySelector('.input-message') as HTMLInputElement;
+    const message = inputElement?.value || '';
+    const ibanRegex = /[A-Z]{2}[\W]?[0-9]{2}[\W]?([A-Z0-9][\W]?){15,34}/g;
+    const ibanMatch = message.match(ibanRegex);
+    const cleanedIban = ibanMatch ? ibanMatch[0].replace(/\W/g, '') : '';
+    setInput(cleanedIban);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Welcome to CopIBAN, a tool designed to help you manage and extract IBAN codes</h1>
+      <p>insert a message containing an IBAN code for processing</p>
+      <input type='text' placeholder='insert the original message' className='input-message'/>
+      <button onClick={handleClick}>V</button>
+      <p>get the IBAN number in various formats from the original message for your convenience</p>
+      <p>{input}</p>
+
+    </div>
   )
 }
 
